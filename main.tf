28bb94d0b8fa8f9738d30021a8a652a83b4e3d52
Tal WOl <talvolcov@hotmail.com>
2025-07-24 20:16:15 +0200

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advanced-terraform-466809" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
